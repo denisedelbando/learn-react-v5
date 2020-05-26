758e0d60475147c3abafe1622c260a8b42713267
denisedelbando <denise.delbando@gmail.com>
2020-05-26 00:07:16 -0400

    Sample hook

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 const App = () => {
-  // return React.createElement(
-  //   "div",
-  //   {
-  //     id: "something-important",
-  //   },
-  //   [
-  //     React.createElement("h1", {}, "Adopt Me!"),
-  //     React.createElement(Pet, {
-  //       name: "Sammy",
-  //       animal: "Dog",
-  //       breed: "Beagle",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Bauer",
-  //       animal: "Dog",
-  //       breed: "Labrador",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Coco",
-  //       animal: "Dog",
-  //       breed: "Unknown",
-  //     }),
-  //   ]
-  // );
   return (
     <div id="something-important">
       <h1>Adopt Me!</h1>
-      <Pet name="Sammy" animal="Dog" breed="Beagle" />
-      <Pet name="Bauer" animal="Dog" breed="Labrador" />
-      <Pet name="Lilly" animal="Cat" breed="Unknown" />
+      <SearchParams />
     </div>
   );
 };
---
+++ src/SearchParams.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+
+const SearchParams = () => {
+  //this is a hook. All hooks has a prefix use. this is by convention
+  //when using hooks, you get an array of two things. First is current state, and the second is an updater function of that particular piece of state
+  //NOTE: hooks should not be inside if or loops. (something that messes the stack trace)
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map((breedStr) => (
+              <option key={breedStr} value={breedStr}>
+                {breedStr}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
